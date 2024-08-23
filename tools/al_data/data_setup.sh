c84266e8d53c69ab3ecc4c719c8c3eba4b3168ed
mohammadmirzaee25 <94175691+mohammadmirzaee25@users.noreply.github.com>
2024-08-23 22:06:59 +0330

    Update data_setup.sh

--- tools/al_data/data_setup.sh
+++ tools/al_data/data_setup.sh
@@ -3,23 +3,23 @@ VOCPATH=$1
 mkdir -p data/VOC0712
 mkdir -p data/VOC0712/annotations
 mkdir -p data/VOC0712/images
-cp /data/VOCdevkit/VOC2007/JPEGImages/*.jpg data/VOC0712/images
-cp /data/VOCdevkit/VOC2012/JPEGImages/*.jpg data/VOC0712/images
+cp /content/PPAL/data/VOCdevkit/VOC2007/JPEGImages/*.jpg data/VOC0712/images
+cp /content/PPAL/data/VOCdevkit/VOC2012/JPEGImages/*.jpg data/VOC0712/images
 cp $VOCPATH data/VOC0712/annotations
 
-mkdir -p data/active_learning/coco/
-mkdir -p data/active_learning/voc/
+mkdir -p /content/PPAL/data/active_learning/coco/
+mkdir -p /content/PPAL/data/active_learning/voc/
 
 python tools/al_data/create_al_dataset.py \
-       --oracle-path data/coco/annotations/instances_train2017.json \
-       --out-root data/active_learning/coco \
-       --n-diff 3 \
-       --n-labeled 2365 \
-       --dataset coco
+       data/coco/annotations/instances_train2017.json \
+       data/active_learning/coco \
+       3 \
+       2365 \
+       coco
 
 python tools/al_data/create_al_dataset.py \
-       --oracle-path data/VOC0712/annotations/trainval_0712.json \
-       --out-root data/active_learning/voc \
-       --n-diff 3 \
-       --n-labeled 827 \
-       --dataset voc
+       data/VOC0712/annotations/trainval_0712.json \
+       data/active_learning/voc \
+       3 \
+       827 \
+       voc
